5e173b81b1ed4ba7e1446fd5e130e005cc1166da
Mohd Armaan <mohammadarmaan533@gmail.com>
2026-05-23 21:55:20 +0530

    fucking error

--- utils/database.py
+++ utils/database.py
@@ -4,7 +4,7 @@ from pymongo.asynchronous.collection import AsyncCollection
 import os
 
 async def init_db():
-    global connector, hcdb, pointsColl, wishlistColl, rewardedColl
+    global connector, pointsColl, wishlistColl, rewardedColl
     connector = pymongo.AsyncMongoClient(os.getenv("MONGO_URI"), serverSelectionTimeoutMS=5000)
     info = connector.server_info()  # Trigger connection to verify credentials and connectivity
     print("Connected to MongoDB")
