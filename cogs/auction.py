d28e71d3fd5e53e97f7de42a7e515166f0bf9e9d
deep095 <107306639+deep095@users.noreply.github.com>
2026-06-01 13:40:24 +0530

    new image testing

--- cogs/auction.py
+++ cogs/auction.py
@@ -64,7 +64,7 @@ class AuctionCog(commands.Cog):
         embed = discord.Embed(title="✦ Heavenly Court Live Auction ✦", color=EMBED_COLOR)
         embed.description = f"### 🎁 {s['item']}"
 
-        embed.set_thumbnail(url="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXlkZTBsaDhldmt3bXB4Zjlrdm5vMm94ZmpmZHhlNTZ4MXB4bXlqayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0w2bnMhOoPuAooKhpD/giphy.gif")
+        embed.set_thumbnail(url="https://cdnb.artstation.com/p/assets/images/images/038/658/167/original/peter-sheff-chest-open03-small.gif")
         
         bid_text = f"## {s['current_bid']} pts" if s["current_bid"] > 0 else "No bids yet!"
         embed.add_field(name="💰 Current Highest Bid", value=bid_text, inline=False)
